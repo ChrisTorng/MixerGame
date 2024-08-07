dc1e3cc97406ec238e0d309c4123c435979dd5cd
ChrisTorng <ChrisTorng@hotmail.com>
2024-08-07 10:13:02 +0800

    Add dev watch mode. Update README.md.

--- README.md
+++ README.md
@@ -1,21 +1,33 @@
 # MixerGame
 Given unknown tracks input, gamer learns how to tune mixer well.
 
+[Live demo](https://christorng.github.io/MixerGame/src/)
+
 # MixerGame 學習混音器操作的遊戲
 
 由未知之多軌音軌輸入，考驗玩家的正確混音能力。
 
-[展示](https://christorng.github.io/MixerGame/src/)
+[線上展示](https://christorng.github.io/MixerGame/src/)
 
-## Requirements
+## Installation
 ```
 npm install -g typescript http-server
+npm install
 ```
 
-## Run
+## Local development
 ```
 npx http-server -p 3000 -c-1
 ```
 Or Windows `run.cmd`.
 
+```
+npm start
+```
+
 Open [http://localhost:3000/src/](http://localhost:3000/src/).
+
+## Before Deploy
+```
+npm run bulid
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    output: {
+      filename: isProduction ? 'bundle.min.js' : 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 9000,
+    },
+    watch: !isProduction,
+  };
 };
